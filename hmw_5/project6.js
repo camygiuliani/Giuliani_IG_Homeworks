99927bd418a2ca2cb6b111fec3101b2408bf24ce
Camilla <camilla.giuliani@live.com>
2025-05-18 17:34:56 +0200

    new functions implemented

--- hmw_5/project6.js
+++ hmw_5/project6.js
@@ -56,11 +56,81 @@ bool IntersectRay( inout HitInfo hit, Ray ray );
 // Shades the given point and returns the computed color.
 vec3 Shade( Material mtl, vec3 position, vec3 normal, vec3 view )
 {
+	//we initialize the color with Black
 	vec3 color = vec3(0,0,0);
+
+	//ambient lighting component--- we set it as 5% of teh diffuse color of the material
+	vec3 ambient_col=mtl.k_d * 0.05;
+
+	// to prevent floating-point inaccuracies
+	float eps=3e-4;
+
+	//we loop thorough all the lights in the scene
 	for ( int i=0; i<NUM_LIGHTS; ++i ) {
-		// TO-DO: Check for shadows
-		// TO-DO: If not shadowed, perform shading using the Blinn model
-		color += mtl.k_d * lights[i].intensity;	// change this line
+		// TODO: Check for shadows
+
+		// we create a ray and we cast it from the surface point to the current light
+		Ray surface_to_light_ray;
+		surface_to_light_ray.dir=normalize(lights[i].pos-position);
+
+		//lights[i].pos-position is a vector starting from intersection point to the light 
+		// then we normalize it to make it a 1-lenght vector
+
+		// Offset the starting point slightly to prevent self-shadowing
+        surface_to_light_ray.pos = position + (surface_to_light_ray.dir) * epsilon;
+
+		//now we want to check if the shadow raw hits something , in that case the point is in shadow
+
+		HitInfo hit={0.0,vec3(0.0),vec3(0.0),{vec3(0.0),vec3(0.0),0}};
+		if(IntersectRay(hit,surface_to_light_ray)){
+			//the point is in shadow so we displey the ambient color
+			color+=ambient_col;
+		}
+		else{ 
+			// TODO: If not shadowed, perform shading using the Blinn model
+
+			 /** *********************
+			 *! Blinnnn Phong 
+			 **********************/
+
+            // Direction from the surface to the light source
+            vec3 lightDir = normalize(lights[i].position - position);
+
+            // Calculate the cosine of the angle between the surface normal and the light direction
+            float cosTheta = dot(normal, lightDir);
+
+            // -------------------------
+            // 1. Diffuse Component
+            // -------------------------
+            // Lambertian reflection model: k_d * I * max(0, cos(theta))
+            vec3 diffuseComponent = mtl.k_d * lights[i].intensity * max(0.0, cosTheta);
+
+            // -------------------------
+            // 2. Specular Component
+            // -------------------------
+            // Blinn-Phong reflection model:
+            // First, compute the half-angle vector (between view and light direction)
+            vec3 halfAngle = normalize(view + lightDir);
+
+            // Compute the cosine of the angle between the normal and the half-angle
+            float cosAlpha = max(0.0, dot(normal, halfAngle));
+
+            // Blinn-Phong specular reflection: k_s * I * (cos(α))^n
+            vec3 specularComponent = mtl.k_s * lights[i].intensity * pow(cosAlpha, mtl.n);
+
+            // -------------------------
+            // 3. Accumulate the Light
+            // -------------------------
+            // Add ambient, diffuse, and specular components to the final color
+            color += ambientComponent + diffuseComponent + specularComponent;
+
+
+		}
+
+
+
+
+		
 	}
 	return color;
 }
@@ -80,22 +150,30 @@ bool IntersectRay( inout HitInfo hit, Ray ray )
 
 
 	for ( int i=0; i<NUM_SPHERES; ++i ) {
-		// TO-DO: Test for ray-sphere intersection
-		// TO-DO: If intersection is found, update the given HitInfo
+		// TODO: Test for ray-sphere intersection
+		// TODO: If intersection is found, update the given HitInfo
 
-		// I take one sphere
+		// I take current sphere
 		Sphere sphere=spheres[i];
 
-		float discriminant= 1; //// TODO
+		// Here I use the ray-sphere intersection formula for the discriminant
+		float discriminant= pow((2*ray.dir*(ray.pos-sphere.center)),2) -
+							4*pow(ray.dir,2)*(((ray.pos-sphere.center)*(ray.pos-sphere.center))-sphere.radius);
 		
-		if(discriminant>=0.0){ // case in which I find the hit 
+		// If the discriminant is non-negative, case in which I find the hit 
+		if(discriminant>=0.0){ 
 
 			// I want the closest ray-sphere intersection
-			float t0=1;
+			float t0=1.0;
 
+			// If the intersection 
+			// is in front of the ray's origin and is closer than previous hits, update HitInfo.
 			if(t0> 0.0 && t0 <=hit.t){
 				foundHit=true;
+
+				// Update the closest intersection distance.
 				hit.t= t0;
+
 				hit.position=ray.pos + (ray.dir * t0);
 				hit.mtl=sphere.mtl;
 				hit.normal= normalize((hit.position - sphere.center)/sphere.radius );
